954352f0602be64b00d9a4ef0e01a405fd13fd4b
MiyaSHs <124814309+MiyaSHs@users.noreply.github.com>
2024-06-15 01:02:11 +0000

    Add files via upload

---
+++ modules/home/gtk.nix
@@ -0,0 +1,47 @@
+{ pkgs, ... }:
+{
+  fonts.fontconfig.enable = true;
+  home.packages = [
+    pkgs.nerdfonts
+    pkgs.twemoji-color-font
+    pkgs.noto-fonts-emoji
+    pkgs.font-awesome
+    pkgs.kdePackages.qt6gtk2
+  ];
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    name = "catppuccin-mocha-sky-cursors";
+    package = pkgs.catppuccin-cursors.mochaSky;
+    size = 24;
+  };
+
+  gtk = {
+    enable = true;
+    catppuccin = {
+      enable = true;
+    };
+
+    #iconTheme = {
+    #  name = "Papirus-Dark";
+    #  package = pkgs.catppuccin-papirus-folders.override {
+    #    flavor = "mocha";
+    #    accent = "sky";
+    #  };
+    #};
+    
+    font = {
+      name = "CaskaydiaCove Nerd Font";
+      size = 11;
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk";
+    style = {
+      name = "qt5gtk2";
+      package = pkgs.adwaita-qt;
+    };
+  };
+}
