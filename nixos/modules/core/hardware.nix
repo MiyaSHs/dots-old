fe63798e4f94c0810892ef4bc3fbf3d2879885fa
MiyaSHs <124814309+MiyaSHs@users.noreply.github.com>
2024-07-25 22:53:19 +0000

    Update hardware.nix

--- nixos/modules/core/hardware.nix
+++ nixos/modules/core/hardware.nix
@@ -6,6 +6,7 @@
       enable32Bit = true;
     };
     enableRedistributableFirmware = true;
+    amdgpu.initrd.enable = true;
   };
   services.system76-scheduler.enable = true;
   hardware.cpu.amd.updateMicrocode = true;
