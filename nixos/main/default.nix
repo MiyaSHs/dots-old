4f28ed4b51b4d93df65bfd59afbae895adc3787b
MiyaSHs <124814309+MiyaSHs@users.noreply.github.com>
2024-07-25 21:46:04 +0000

    Update default.nix

--- nixos/main/default.nix
+++ nixos/main/default.nix
@@ -4,9 +4,4 @@
     ./hardware-configuration.nix
     ./../modules/core
   ];
-  
-  services.system76-scheduler.enable = true;
-  programs.corectrl.gpuOverclock.enable = true;
-  powerManagement.cpuFreqGovernor = "performance";
-  hardware.cpu.amd.updateMicrocode = true;
 }
