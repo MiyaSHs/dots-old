85a83778272aa4969542fec39fa35718ec61b518
MiyaSHs <124814309+MiyaSHs@users.noreply.github.com>
2024-07-25 22:38:47 +0000

    Update default.nix

--- nixos/modules/core/default.nix
+++ nixos/modules/core/default.nix
@@ -4,17 +4,14 @@
        [ (import ./catppuccin.nix) ]
     ++ [ (import ./bootloader.nix) ]
     ++ [ (import ./hardware.nix) ]
-    ++ [ (import ./xserver.nix) ]
     ++ [ (import ./network.nix) ]
-    ++ [ (import ./pipewire.nix) ]
-    ++ [ (import ./program.nix) ]
+    ++ [ (import ./sound.nix) ]
     ++ [ (import ./security.nix) ]
     ++ [ (import ./services.nix) ]
     ++ [ (import ./system.nix) ]
     ++ [ (import ./user.nix) ]
     ++ [ (import ./wayland.nix) ]
     ++ [ (import ./virtualization.nix) ]
-    ++ [ (import ./mouse.nix) ]
     ++ [ (import ./printing.nix) ]
     ++ [ (import ./steam.nix) ];
 }
